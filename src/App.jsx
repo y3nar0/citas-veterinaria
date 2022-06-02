ab172c97dca65a5ce5fde197d291cba85aa482b7
Genaro OG <genaro.osorio@icoddes.com>
2022-06-02 16:43:05 -0500

    config tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </>
+  )
+}
+
+export default App
