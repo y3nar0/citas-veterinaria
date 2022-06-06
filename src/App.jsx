e47adfb268feef5322d5557259464a94a0c8fa2e
Genaro OG <genaro.osorio@icoddes.com>
2022-06-06 01:27:20 -0500

    Add Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+  const LS = JSON.parse(localStorage.getItem('pacientes')) ?? []
 
-  const [pacientes, setPacientes] = useState([])
+  const [pacientes, setPacientes] = useState(LS)
   const [paciente, setPaciente] = useState({})
+
+  useEffect( () => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
 
   const eliminarPaciente = id => {
     // console.log('Eliminando ', id)
